353c701fdb9077d5ed8c7e13a2779452856d9f9c
REDFOX39 <daniil.fox1998@yandex.ru>
2022-03-31 19:52:02 +0300

    Dev 0.3 > case 6x

--- dist/js/script.min.js
+++ dist/js/script.min.js
@@ -1,5 +1,21 @@
-let headerTitle = $(".header__title");
+let buttons = document.querySelectorAll(".deco-btn__btn");
 
-console.log(headerTitle);
+buttons.forEach(btn => {
+	btn.addEventListener('click', function(e) {
+
+		let x = e.pageX - e.target.offsetLeft;
+		let y = e.pageY - e.target.offsetTop;
+
+		let ripples = document.createElement('span');
+		ripples.classList.add('ripples');
+		ripples.style.left = x + 'px';
+		ripples.style.top = y + 'px';
+		this.appendChild(ripples);
+
+		setTimeout(() => {
+			ripples.remove();
+		},500)
+	})
+});
 
 //<!DOCTYPE Liky>
